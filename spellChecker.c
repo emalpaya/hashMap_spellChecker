e837b74e422f1e925df385175873d44577dc8ded
Eva Malpaya <malpayae@oregonstate.edu>
2020-02-26 21:23:55 -0800

    Initial commit; transfer from local repo

---
+++ spellChecker.c
@@ -0,0 +1,98 @@
+#include "hashMap.h"
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * Allocates a string for the next word in the file and returns it. This string
+ * is null terminated. Returns NULL after reaching the end of the file.
+ * @param file
+ * @return Allocated string or NULL.
+ */
+char* nextWord(FILE* file)
+{
+    int maxLength = 16;
+    int length = 0;
+    char* word = malloc(sizeof(char) * maxLength);
+    while (1)
+    {
+        char c = fgetc(file);
+        if ((c >= '0' && c <= '9') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            c == '\'')
+        {
+            if (length + 1 >= maxLength)
+            {
+                maxLength *= 2;
+                word = realloc(word, maxLength);
+            }
+            word[length] = c;
+            length++;
+        }
+        else if (length > 0 || c == EOF)
+        {
+            break;
+        }
+    }
+    if (length == 0)
+    {
+        free(word);
+        return NULL;
+    }
+    word[length] = '\0';
+    return word;
+}
+
+/**
+ * Loads the contents of the file into the hash map.
+ * @param file
+ * @param map
+ */
+void loadDictionary(FILE* file, HashMap* map)
+{
+    // FIXME: implement
+}
+
+/**
+ * Checks the spelling of the word provded by the user. If the word is spelled incorrectly,
+ * print the 5 closest words as determined by a metric like the Levenshtein distance.
+ * Otherwise, indicate that the provded word is spelled correctly. Use dictionary.txt to
+ * create the dictionary.
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, const char** argv)
+{
+    // FIXME: implement
+    HashMap* map = hashMapNew(1000);
+
+    FILE* file = fopen("dictionary.txt", "r");
+    clock_t timer = clock();
+    loadDictionary(file, map);
+    timer = clock() - timer;
+    printf("Dictionary loaded in %f seconds\n", (float)timer / (float)CLOCKS_PER_SEC);
+    fclose(file);
+
+    char inputBuffer[256];
+    int quit = 0;
+    while (!quit)
+    {
+        printf("Enter a word or \"quit\" to quit: ");
+        scanf("%s", inputBuffer);
+
+        // Implement the spell checker code here..
+
+        if (strcmp(inputBuffer, "quit") == 0)
+        {
+            quit = 1;
+        }
+    }
+
+    hashMapDelete(map);
+    return 0;
+}
